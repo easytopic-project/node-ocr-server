a8f373f543292df9c80291b8635eb5865409ba54
Maxwell Souza <maxwell.carvalho@capgemini.com>
2021-02-10 21:31:06 -0300

    FIX amqp start connection

--- src/env.js
+++ src/env.js
@@ -3,6 +3,8 @@ export const {
     QUEUE_SERVER = 'localhost:5672',
     INPUT_QUEUE_NAME = 'ocr-in',
     OUTPUT_QUEUE_NAME = 'ocr-out',
-    FILES_PATH = '/temp',   // Folder where temporally files will be stored
-    FILES_SERVER = 'localhost:3000',      // Files server
+    FILES_PATH = '/temp',               // Folder where temporally files will be stored
+    FILES_SERVER = 'localhost:3000',    // Files server
+    RECONNECT_TIMEOUT = 3000,           // Time to retry to connect to AMQP
+    RECONNECT_MAX = 30,                 // Maximum attempts to reconnect
 } = process.env
--- src/index.js
+++ src/index.js
@@ -4,22 +4,39 @@ import {
     INPUT_QUEUE_NAME as qIn,
     OUTPUT_QUEUE_NAME as qOut,
     FILES_PATH,
-    FILES_SERVER
+    FILES_SERVER,
+    RECONNECT_TIMEOUT,
+    RECONNECT_MAX,
 } from './env';
 import tesseract from 'node-tesseract-ocr'
 import amqp from 'amqplib'
 import { downloadFile, deleteFile } from './files';
-
+let retries = 0
 const config = {
     lang: "eng",
     oem: 1,
     psm: 3,
 }
 
+const asyncTimeout = time => new Promise(resolve => setTimeout(resolve, time))
+
+async function getConnection() {
+    try {
+        return await amqp.connect(`amqp://${QUEUE_SERVER}`)
+    }
+    catch (err) {
+        retries++;
+        if (retries > RECONNECT_MAX) throw err
+        console.warn(`Connection to ${QUEUE_SERVER} failed (attempt ${retries}). Retrying in ${RECONNECT_TIMEOUT / 1000} seconds...`)
+        await asyncTimeout(RECONNECT_TIMEOUT);
+        return await getConnection()
+    }
+}
+
 
 
 async function main() {
-    const connection = await amqp.connect(`amqp://${QUEUE_SERVER}`)
+    const connection = await getConnection()
     const ch = await connection.createChannel()
     await ch.assertQueue(qOut, { durable: true })
     await ch.assertQueue(qIn, { durable: true })
